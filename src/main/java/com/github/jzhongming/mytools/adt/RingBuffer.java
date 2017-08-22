5231b128a83eb73e6f228221fc2a6978dec5ac2c
j.zhongming@gmail.com <j.zhongming@gmail.com>
2017-08-22 17:15:37 +0800

    增加简单Ringbuffer

---
+++ src/main/java/com/github/jzhongming/mytools/adt/RingBuffer.java
@@ -0,0 +1,67 @@
+package com.github.jzhongming.mytools.adt;
+
+public class RingBuffer {
+	private final static int bufferSize = 1024;
+	private String[] buffer = new String[bufferSize];
+	private int head = 0;
+	private int tail = 0;
+
+	private Boolean empty() {
+		return head == tail;
+	}
+
+	private Boolean full() {
+		return (tail + 1) % bufferSize == head;
+	}
+
+	public Boolean put(String v) {
+		if (full()) {
+			return false;
+		}
+		buffer[tail] = v;
+		tail = (tail + 1) % bufferSize;
+		return true;
+	}
+
+	public String get() {
+		if (empty()) {
+			return null;
+		}
+		String result = buffer[head];
+		head = (head + 1) % bufferSize;
+		return result;
+	}
+
+	public String[] getAll() {
+		if (empty()) {
+			return new String[0];
+		}
+		int copyTail = tail;
+		int cnt = head < copyTail ? copyTail - head : bufferSize - head + copyTail;
+		String[] result = new String[cnt];
+		if (head < copyTail) {
+			for (int i = head; i < copyTail; i++) {
+				result[i - head] = buffer[i];
+			}
+		} else {
+			for (int i = head; i < bufferSize; i++) {
+				result[i - head] = buffer[i];
+			}
+			for (int i = 0; i < copyTail; i++) {
+				result[bufferSize - head + i] = buffer[i];
+			}
+		}
+		head = copyTail;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		RingBuffer rBuffer = new RingBuffer();
+		for( int i=0; i<1023; i++) {
+			System.out.println(rBuffer.put(""+i));
+		}
+		System.out.println(rBuffer.put("kkk"));
+		System.out.println(rBuffer.getAll().length);
+		System.out.println(rBuffer.put("kkk"));
+	}
+}
